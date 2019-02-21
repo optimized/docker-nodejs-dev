d460fdf0051f1a27f68c04ec95ad83b589d76e34
Noel Jackson <n@noeljackson.com>
2019-02-21 14:46:13 -0500

    Version: v10.15.1

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,8 @@ USER_ID=${CURRENT_UID:-9001}
 
 echo "Starting with UID: $USER_ID"
 echo "export CURRENT_UID=\$(id -u)"
-useradd --shell /bin/bash -u $USER_ID -o -c "" -m user
-export HOME=/home/user
-
-chown -R $(id -u $USER_ID):$(id -g $USER_ID) /usr/src/app
+    # useradd --shell /bin/bash -u $USER_ID -o -c "" -m user
+usermod --shell /bin/bash -u $USER_ID -m node
+export HOME=/home/node
+chown -R $(id -u $USER_ID) /usr/src/app/ /usr/src/app/.*
 exec "$@"
